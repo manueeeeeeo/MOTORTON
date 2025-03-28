e1fbcb46816452e3aa503a19e0b7ca7684f88796
manueeeeeeo <manu.engenios@gamil.com>
2025-03-28 22:30:57 +0100

    Agregada la pantalla de carga principal para tratar los datos la autentificación del usuario

---
+++ app/src/main/java/com/clase/motorton/ui/carga/PantallaCarga.java
@@ -0,0 +1,36 @@
+package com.clase.motorton.ui.carga;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.clase.motorton.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PantallaCarga extends AppCompatActivity {
+    private TextView welcomeText = null;
+    private ProgressBar progressBar = null;
+    private Toast mensajeToast = null;
+    private FirebaseAuth auth = null;
+    private FirebaseFirestore db = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pantalla_carga);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
