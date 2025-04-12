bcff28ccbb1597d75b24fc1299f51363f6397e7f
manueeeeeeo <manu.engenios@gamil.com>
2025-04-12 15:33:29 +0200

    Diseño principal de la pantalla de creación del perfil creado, además de agregada la funcionalidad del calendario despegable para la fecha de nacimiento.

--- app/src/main/java/com/clase/motorton/ui/perfil/CreacionPerfil.java
+++ app/src/main/java/com/clase/motorton/ui/perfil/CreacionPerfil.java
@@ -1,6 +1,8 @@
 package com.clase.motorton.ui.perfil;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.clase.motorton.R;
 
+import java.util.Calendar;
+
 public class CreacionPerfil extends AppCompatActivity {
+    private EditText editFechaNacimiento = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,26 @@ public class CreacionPerfil extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        editFechaNacimiento = findViewById(R.id.dob_input);
+
+        editFechaNacimiento.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    CreacionPerfil.this,
+                    (view, selectedYear, selectedMonth, selectedDay) -> {
+                        String selectedDate = String.format("%04d-%02d-%02d", selectedYear, selectedMonth + 1, selectedDay);
+                        editFechaNacimiento.setText(selectedDate);
+                    },
+                    year, month, day
+            );
+
+            datePickerDialog.show();
         });
     }
 }
