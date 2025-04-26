75bf6c538c40981f813c3c03497a7641dce482de
manueeeeeeo <manu.engenios@gamil.com>
2025-04-26 12:37:33 +0200

    Inicio con la lógica para la pantalla de visualziación de datos de otros usuarios. Declarando variables incializando todo, etc.

--- app/src/main/java/com/clase/motorton/ui/perfil/InfoOtroPerfilFragment.java
+++ app/src/main/java/com/clase/motorton/ui/perfil/InfoOtroPerfilFragment.java
@@ -7,60 +7,74 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.clase.motorton.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InfoOtroPerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InfoOtroPerfilFragment extends Fragment {
+    // Variable para manejar los Toast de está actividad
+    private Toast mensajeToast = null;
+    private TextView textUsername = null;
+    private TextView textEdad = null;
+    private TextView textNombreCompleto = null;
+    private TextView textAniosConduciendo = null;
+    private TextView textUbicacion = null;
+    private ImageView imagenPerfil = null;
+    private Button btnLike = null;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InfoOtroPerfilFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InfoOtroPerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InfoOtroPerfilFragment newInstance(String param1, String param2) {
-        InfoOtroPerfilFragment fragment = new InfoOtroPerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // Variable para manejar el autenticado de firebase
+    private FirebaseAuth mAuth = null;
+    // Variable para manejar la base de datos de firebase
+    private FirebaseFirestore db = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_otro_perfil, container, false);
+        View root = inflater.inflate(R.layout.fragment_info_otro_perfil, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        textUsername = (TextView) root.findViewById(R.id.textViewUsernameOtroUsuario);
+        textUbicacion = (TextView) root.findViewById(R.id.textViewUbicacionOtroUsuario);
+        textAniosConduciendo = (TextView) root.findViewById(R.id.textViewAnosConduciendoOtroUsuario);
+        textEdad = (TextView) root.findViewById(R.id.textViewEdadOtroUsuario);
+        textNombreCompleto = (TextView) root.findViewById(R.id.textViewNombreCompletoOtroUsuario);
+        imagenPerfil = (ImageView) root.findViewById(R.id.imageViewPerfilOtroUsuario);
+        btnLike = (Button) root.findViewById(R.id.buttonLike);
+
+        btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return root;
+    }
+
+    /**
+     * @param mensaje
+     * Método para ir matando los Toast y mostrar todos en el mismo para evitar
+     * colas de Toasts y que se ralentice el dispositivo
+     */
+    public void showToast(String mensaje){
+        if (this != null){
+            // Comprobamos si existe algun toast cargado en el toast de la variable global
+            if (mensajeToast != null) { // En caso de que si que exista
+                mensajeToast.cancel(); // Le cancelamos, es decir le "matamos"
+            }
+
+            // Creamos un nuevo Toast con el mensaje que nos dan de argumento en el método
+            mensajeToast = Toast.makeText(getContext(), mensaje, Toast.LENGTH_SHORT);
+            // Mostramos dicho Toast
+            mensajeToast.show();
+        }
     }
 }
