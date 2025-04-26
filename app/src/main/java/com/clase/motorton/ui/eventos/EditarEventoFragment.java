6c60f26efb432b2cc71b534d9c97b53db635c9b2
manueeeeeeo <manu.engenios@gamil.com>
2025-04-26 19:03:14 +0200

    Agregado el nuevo fragmento para editar un evento tuyo.

---
+++ app/src/main/java/com/clase/motorton/ui/eventos/EditarEventoFragment.java
@@ -0,0 +1,23 @@
+package com.clase.motorton.ui.eventos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.clase.motorton.R;
+
+public class EditarEventoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_editar_evento, container, false);
+
+        return view;
+    }
+}
