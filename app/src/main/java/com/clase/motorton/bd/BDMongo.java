480973ac40073547bb8e9c134de075366b003532
manueeeeeeo <manu.engenios@gamil.com>
2025-05-10 19:34:21 +0200

    Prosigo con el desarrollo de la clase para hacer las consultas y obtener las listas de marcas y modelos a mongodb.

--- app/src/main/java/com/clase/motorton/bd/BDMongo.java
+++ app/src/main/java/com/clase/motorton/bd/BDMongo.java
@@ -1,4 +1,28 @@
 package com.clase.motorton.bd;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class BDMongo {
+    MongoClient mongoClient = MongoConection.getMongoClient();
+    MongoDatabase database = mongoClient.getDatabase("vehiculosApi");
+    MongoCollection<Document> collection = database.getCollection("vehiculos");
+
+    public List<String> ObtenerMarcas(String tipoVeh){
+        return null;
+    }
+
+    public List<String> ObtenerModelos(String tipoVeh, String marca){
+        return null;
+    }
 }
